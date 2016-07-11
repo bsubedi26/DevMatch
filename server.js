5fdb1906c0713d60138bb9d75f442fb85e944c69
Brian Burnside <bcburnside58@gmail.com>
2016-07-11 12:48:59 -0400

    server good to go

---
+++ server.js
@@ -0,0 +1,39 @@
+//dependancies needed for server file
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var connection = require('./config/connection.js');
+var path = require('path');
+
+var app = express();
+
+//============== NOTE: not sure if both of these are needed ===================
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+//To access css
+app.use(express.static('/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ 
+	extended: false 
+}));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+/*Our controller file as per the MVC standard
+			//require('./controllers/controller.js')(app);
+=============Uncomment this section once controller.js file is complete===================
+*/
+
+
+var PORT = 8080;
+
+app.listen(PORT, function() {
+    console.log("Listening on %d", PORT);
+});
