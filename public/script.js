616615d735fccbf39adda3738bedc54fde78107a
Danielle Bowman <dcbowman6@yahoo.com>
2016-07-10 21:56:42 -0400

    script pseudo code

---
+++ public/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+//console.log("js loaded");
+
+
+
+
+
+//submit button function to capture user info and calculate personality match
+
+//info pulled from form and entered into database
+
+
+
+//function for personality
+//pull the values from the 5 questions to calculate the %
+
+//add 1% for every check box that's added
+
+//result is % matching variable
+
+//info enters column into database
+
+
+});
