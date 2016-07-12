b9b0bc737df6d8dc21f9c95d9b772ec375544c3d
Brian Burnside <bcburnside58@gmail.com>
2016-07-12 14:20:29 -0400

    bare bones of orm

--- config/orm.js
+++ config/orm.js
@@ -4,15 +4,41 @@
 var connection = require('../config/connection.js');
 
 var orm = {
-    selectFramework: function(tableInput, colToSearch, valOfCol) {
+	//can use this to select all data from any table
+    selectAll: function(table) {
     	return new Promise(function(resolve, reject) {
-	        var queryString = 'SELECT * FROM ' + tableInput + ' WHERE ' + colToSearch + ' = ?';
-
-	        connection.query(queryString, [valOfCol], function(err, result) {
+	        var queryString = 'SELECT * FROM ' + table;
+	        connection.query(queryString, function(err, result) {
 	            resolve(result);
 	        });
     	});
     }
+    /* For the next 2 functions below, I need to learn how to tie the User table with the skills table 
+    together by id */
+    addUsers: function(id){
+    	return new Promise(function(resolve, reject){
+    		var queryString = 'INSERT INTO users WHERE id = ?';
+    	})
+    }
+
+    addSkills: function(id){
+    	return new Promise(function(resolve, reject){
+    		var queryString = 'INSERT INTO skills WHERE id = ?';
+    		connection.query(queryString, function(err, result){
+    			resolve(result);
+    		})
+    	})
+    }
+    
+   	addCompanyScores: function(score){
+   		return new Promise(function(resolve, reject){
+   			var queryString = 'INSERT INTO recruiter';
+   			connection.query(queryString, function(err, result){
+   				resolve(result);
+   			})
+   		})
+   	}
+
 };
 
 module.exports = orm;
