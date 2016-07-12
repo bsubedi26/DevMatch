a314c07bc1f0258c7dc62d827506f9f0467ebb4e
shainaveloz <shaina.veloz@gmail.com>
2016-07-11 20:13:47 -0400

    merge

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(first_name, last_name) VALUES('Tim', 'Cumminsky');
+INSERT INTO users(first_name, last_name) VALUES('James', 'Harper');
+INSERT INTO users(first_name, last_name) VALUES('Sasha', 'Grotto');
