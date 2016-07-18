e94edc472cc64a6b03d9beb123027d243338d66a
Brian Burnside <bcburnside58@gmail.com>
2016-07-18 18:45:57 -0400

    revised master

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-
-//dependancies needed for server file
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var connection = require('./config/connection.js');
 var path = require('path');
+var passport = require('passport');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
-
 //============== NOTE: not sure if both of these are needed ===================
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
-//To access css
-app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ 
-	extended: false 
-}));
+// passport for user authentication
+require('./config/passport')(passport); 
+
+// BodyParser interprets data sent to the server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
+// cookie parser for user authentication
+app.use(cookieParser());
+// session configuration
+app.use(session({
+	secret: 'devmatch',
+	resave: true,
+	saveUninitialized: true
+ } ));
+
+// use passport authentication middleware 
+app.use(passport.initialize());
+app.use(passport.session());
 
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'))
@@ -28,9 +42,8 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-//Our controller file as per the MVC standard
-require('./controllers/controller.js')(app);
-
+//Require the controller file
+require('./controllers/controller.js')(app, passport);
 
 var PORT = process.env.PORT || 8080;
 
