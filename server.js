5b6e8e24b1c76880c3dc9194717fb3dd8e957518
Bibek Subedi <bsubedi26@gmail.com>
2016-07-20 14:00:40 -0400

    new branch to test routes/login with bootstrap

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var passport = require('passport');
 var session  = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('connect-flash');
-
 var app = express();
 
 //============== NOTE: not sure if both of these are needed ===================
@@ -26,9 +25,9 @@ app.use(cookieParser());
 // session configuration
 app.use(session({
 	secret: 'devmatch',
-	cookie: { maxAge: 60000 },
+	cookie: { maxAge: 100000 },
 	resave: true,
-	saveUninitialized: true
+	saveUninitialized: true,
  } ));
 
 //flash is used to show a message on an incorrect login
