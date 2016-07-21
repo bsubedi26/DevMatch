868140d45f9930082709d1e4c647f85ba415e191
shainaveloz <shaina.veloz@gmail.com>
2016-07-20 21:36:06 -0400

    changed the search function

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,10 @@
+var admin = require('./views/partials/admin.handlebars');
+
 $('#submit').on('click',function(event){
     var search = $(this).data('applicants');
     for (var i = 0; i < users.length; i ++){
-    if (search == company.score){
-    return
-    }
+        if (users.skills == true){
+        return ('<div>' + '<ul>' + {{#each users}} + '</ul>' + '</div>')
+        }
     }
 };
