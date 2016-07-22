00fe1f160d766d14fc707a0283c8f3386326493d
Brian Burnside <bcburnside58@gmail.com>
2016-07-22 00:00:08 -0400

    JOIN tables, personality type working

--- config/orm.js
+++ config/orm.js
@@ -8,7 +8,7 @@ var orm = {
     viewAll: function(table) {
     	return new Promise(function(resolve, reject) {
     		//may need to do A.ID instead of A.Key format, 'SELECT * FROM ' + table + ' A LEFT JOIN skills B ON A.ID = B.ID'
-	        var queryString = 'SELECT * FROM ' + table;
+	        var queryString = 'SELECT * FROM ' + table + ' A LEFT JOIN scores B ON A.id = B.id';
 	        connection.query(queryString, function(err, result) {
 	            resolve(result);
 	        });
