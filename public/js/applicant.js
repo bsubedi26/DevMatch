e1f69c065894308ded57b8ef6e9cb88ad87e8b8c
Brian Burnside <bcburnside58@gmail.com>
2016-07-21 22:10:25 -0400

    updated application.js

--- public/js/applicant.js
+++ public/js/applicant.js
@@ -57,7 +57,7 @@ if (score >= 75) {
          return style;
 
 //================================The posts to server side====================================
-	$.post('/createSkills', {personality_type: style});
+$.post('/score', {personality_type: style});
 
    //jQuery verison - will delete once lodash works 100%
 	
